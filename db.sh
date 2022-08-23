8e7248f0a47188a2a3b2d2e5f89da25bd9bc4914
Vlad Lan <vladislav_lanskikh@epam.com>
2022-08-23 18:43:01 +0200

    Added restore command

--- db.sh
+++ db.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 
+DB_FILE_PATH=./data/users.db
+
 add() {
 	read -p "Type username " username
 	read -p "Type role " role
 	mkdir -p data
-	echo "$username,$role" >> ./data/users.db
+	echo "$username,$role" >> "$DB_FILE_PATH"
 }
 
 backup() {
-	now=$(date +"%m_%d_%Y_%H_%M_%S")
+	now=$(date +"%d_%m_%Y_%H_%M_%S")
 	mkdir -p backups
-	echo "now=$now"
-	cp ./data/users.db "./backups/$now-users.db.backup"
+	cp "$DB_FILE_PATH" "./backups/$now-users.db.backup"
+}
+
+restore () {
+	if test -f "$DB_FILE_PATH"; then
+    	lastBackup=$(ls -rtc ./backups | tail -1)
+		content=$(cat "./backups/$lastBackup")
+		echo "$content" > "$DB_FILE_PATH"
+	else
+		echo "No backup file found"
+	fi	
 }
 
 case $1 in
 	"add") add;;
 	"backup") backup;;
+	"restore") restore;;
 	"help") echo "Available commands are: add, backup, find, list, help";;
 	*) echo "'$1' is not supported script command. Run it with help argument to see which arguments are available"
 esac
-
