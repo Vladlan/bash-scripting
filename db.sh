27680a7ebb7b14f02233dc07b2f0ffcdb89c1ad0
Vlad Lan <vladislav_lanskikh@epam.com>
2022-08-23 17:55:52 +0200

    Added help argument

--- db.sh
+++ db.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-
 add() {
 	read -p "Type username " username
 	read -p "Type role " role
 	echo "$username,$role" >> ./data/users.db
 }
 
-if [[ $1 == "add" ]]; then
-	echo "argument $1"
-    add
-else
-	echo "argument $1"
-	echo "Not supported script command. Run it with help argument to see which arguments are available"
-fi
+case $1 in
+	"add") add;;
+	"help") echo "Available commands are: add, backup, find, list, help";;
+	*) echo "'$1' is not supported script command. Run it with help argument to see which arguments are available"
+esac
+
