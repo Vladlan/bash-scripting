796cad69a8f3e11ac866288648923f6843e98abd
Vlad Lan <vladislav_lanskikh@epam.com>
2022-08-24 14:45:01 +0200

    Added check for existing zip file to remove it before build

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
+FE_DIST_ZIP_PATH=./../shop-angular-cloudfront/dist/client-app.zip
+
+checkIfFileExists() {
+	if test -f "$1"; then
+		echo 1
+	else
+		echo 0
+	fi
+}
+
+if [ $(checkIfFileExists "$FE_DIST_ZIP_PATH") -eq 1 ]; then
+    echo "$FE_DIST_ZIP_PATH"
+	rm "$FE_DIST_ZIP_PATH"
+fi
+
 npm run build --prefix ./../shop-angular-cloudfront/ -- --configuration production
 zip -r ./../shop-angular-cloudfront/dist/client-app.zip ./../shop-angular-cloudfront/dist/
