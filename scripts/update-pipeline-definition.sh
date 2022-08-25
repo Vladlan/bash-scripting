374ed68861b036633a4edc513420dfc64c635e8f
Vlad Lan <vladislav_lanskikh@epam.com>
2022-08-25 12:00:03 +0200

    Finished update-pipline script

--- scripts/update-pipeline-definition.sh
+++ scripts/update-pipeline-definition.sh
@@ -6,14 +6,63 @@ then
     exit 1
 fi
 
-PIPLINE_FILE_PATH="./pipline.json"
-# NOW=$(date +"%d_%m_%Y_%H_%M_%S")
-NOW="2"
+PIPLINE_FILE_PATH=""
+NOW=$(date +"%d_%m_%Y_%H_%M_%S")
 BRANCH=$(git symbolic-ref -q HEAD)
 BRANCH=${BRANCH##refs/heads/}
 OWNER=$(git config --global --get user.email)
 POLL_FOR_SOURCE_CHANGES=false
 CONFIGURATION='production'
+
+PARAMS=""
+while (( "$#" )); do
+  case "$1" in
+    -c|--configuration)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        CONFIGURATION=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -o|--owner)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        OWNER=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -b|--branch)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        BRANCH=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -p|--poll-for-source-changes)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        POLL_FOR_SOURCE_CHANGES=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *)
+      PIPLINE_FILE_PATH="$1"
+      shift
+      ;;
+  esac
+done
 
 checkIfFileExists() {
 	if test -f "$1"; then
@@ -24,13 +73,13 @@ checkIfFileExists() {
 }
 
 if [ $(checkIfFileExists "$PIPLINE_FILE_PATH") == 0 ]; then
-	echo "File '$PIPLINE_FILE_PATH' does not exists"
+	echo "Pipline file on path '$PIPLINE_FILE_PATH' does not exists"
     exit 1
 fi
 
-echo $(cat "./pipline.json" | jq 'del(.metadata)') > "./pipline-$NOW.json"
-echo $(jq ".pipeline.version = .pipeline.version+1" "./pipline-$NOW.json") > "./pipline-$NOW.json"
-echo $(jq --arg BRANCH "$BRANCH" '.pipeline.stages[0].actions[0].configuration.Branch = $BRANCH' "./pipline-$NOW.json") > "./pipline-$NOW.json"
-echo $(jq --arg OWNER "$OWNER" '.pipeline.stages[0].actions[0].configuration.Owner = $OWNER' "./pipline-$NOW.json") > "./pipline-$NOW.json"
-echo $(jq ".pipeline.stages[0].actions[0].configuration.PollForSourceChanges = $POLL_FOR_SOURCE_CHANGES" "./pipline-$NOW.json") > "./pipline-$NOW.json"
-echo $(jq --arg CONFIGURATION "$CONFIGURATION" '((.. | .EnvironmentVariables?) |= if . == null then empty else gsub("{{BUILD_CONFIGURATION value}}"; $CONFIGURATION) end )' "./pipline-$NOW.json") > "./pipline-$NOW.json"
+echo $(cat "$PIPLINE_FILE_PATH" | jq 'del(.metadata)') > "./pipeline-$NOW.json"
+echo $(jq ".pipeline.version = .pipeline.version+1" "./pipeline-$NOW.json") > "./pipeline-$NOW.json"
+echo $(jq --arg BRANCH "$BRANCH" '.pipeline.stages[0].actions[0].configuration.Branch = $BRANCH' "./pipeline-$NOW.json") > "./pipeline-$NOW.json"
+echo $(jq --arg OWNER "$OWNER" '.pipeline.stages[0].actions[0].configuration.Owner = $OWNER' "./pipeline-$NOW.json") > "./pipeline-$NOW.json"
+echo $(jq ".pipeline.stages[0].actions[0].configuration.PollForSourceChanges = $POLL_FOR_SOURCE_CHANGES" "./pipeline-$NOW.json") > "./pipeline-$NOW.json"
+echo $(jq --arg CONFIGURATION "$CONFIGURATION" '((.. | .EnvironmentVariables?) |= if . == null then empty else gsub("{{BUILD_CONFIGURATION value}}"; $CONFIGURATION) end )' "./pipeline-$NOW.json") > "./pipeline-$NOW.json"
