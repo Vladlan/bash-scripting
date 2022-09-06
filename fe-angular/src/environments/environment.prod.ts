3b18d9327b4d283883535792f2e03b959fcf5f66
Vlad Lan <vladislav_lanskikh@epam.com>
2022-09-06 10:48:23 +0200

    Fix api endpoints to https

--- fe-angular/src/environments/environment.prod.ts
+++ fe-angular/src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'http://192.168.0.3/api',
-    order: 'http://192.168.0.3/api',
-    import: 'http://192.168.0.3/api',
-    bff: 'http://192.168.0.3/api',
-    cart: 'http://192.168.0.3/api',
+    product: 'https://192.168.0.3/api',
+    order: 'https://192.168.0.3/api',
+    import: 'https://192.168.0.3/api',
+    bff: 'https://192.168.0.3/api',
+    cart: 'https://192.168.0.3/api',
   },
   apiEndpointsEnabled: {
     product: true,
