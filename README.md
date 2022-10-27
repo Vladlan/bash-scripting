648bce1e11125e168843ecd1eb4c63c4b8010b45
Vlad Lan <vladislav_lanskikh@epam.com>
2022-10-27 20:25:45 +0200

    Improved lab2

---
+++ README.md
@@ -0,0 +1,6 @@
+Prerequisites:
+
+1. Install vagrant
+2. Ran `vagrant up` to up virtual ubuntu server on your local machine
+3. (Optionally) Ran `vagrant ssh` to connect to virtual ubuntu server over ssh
+4. Ran `./deploy.sh` to deploy be and fe to your virtual ubuntu server
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
+FE_DIST_PATH=./fe-angular/dist/
 FE_DIST_ZIP_PATH=./fe-angular/dist/be.zip
 BE_DIST_ZIP_PATH=./nestjs-rest-api/dist/be.zip
 REMOTE_SERVER_IP=192.168.0.3
 REMOTE_SERVER_USER=vagrant
 PRIVATE_KEY_LOCATION=.vagrant/machines/default/virtualbox/private_key
+BE_ENV_FILE=./nestjs-rest-api/.env.prod
+
+
+checkIfNodeExists() {
+	if which node > /dev/null
+    then
+        echo "NodeJS version:"
+		node -v
+    else
+        echo "NodeJS is not installed"
+		exit 1
+    fi
+}
+
+checkIfNodeExists
+
 
 checkIfFileExists() {
 	if test -f "$1"; then
@@ -13,12 +30,28 @@ checkIfFileExists() {
 	fi
 }
 
+stopScriptIfPathDoesNotExist() {
+	if test -f "$1"; then
+		echo ""
+	else
+		echo "Path '$1' does not exist!"
+		exit 1
+	fi
+}
+
+stopScriptIfPathDoesNotExist "$BE_ENV_FILE"
+
 echo 'Building FE...'
 if [ $(checkIfFileExists "$FE_DIST_ZIP_PATH") -eq 1 ]; then
 	rm "$FE_DIST_ZIP_PATH"
 fi
 npm run build --prefix ./fe-angular/ -- --configuration production
-cd ./fe-angular/dist/app/
+
+stopScriptIfPathDoesNotExist "$FE_DIST_PATH"
+
+cd FE_DIST_PATH
+
+echo 'Ziping FE...'
 zip -r ../fe.zip *
 cd ./../../../
 
@@ -26,11 +59,12 @@ echo 'Ziping BE...'
 if [ $(checkIfFileExists "$BE_DIST_ZIP_PATH") -eq 0 ]; then
 	mkdir ./nestjs-rest-api/dist
 fi
-zip -r $BE_DIST_ZIP_PATH ./nestjs-rest-api/package.json ./nestjs-rest-api/package-lock.json ./nestjs-rest-api/tsconfig.json ./nestjs-rest-api/tsconfig.build.json ./nestjs-rest-api/.env ./nestjs-rest-api/src/ ./nestjs-rest-api/nest-cli.json  
+
+zip -r $BE_DIST_ZIP_PATH ./nestjs-rest-api/package.json ./nestjs-rest-api/package-lock.json ./nestjs-rest-api/tsconfig.json ./nestjs-rest-api/tsconfig.build.json ./nestjs-rest-api/.env.prod ./nestjs-rest-api/src/ ./nestjs-rest-api/nest-cli.json  
 
 echo 'Sending files to a server...'
 scp -i "$PRIVATE_KEY_LOCATION" ./fe-angular/dist/fe.zip "$REMOTE_SERVER_USER@$REMOTE_SERVER_IP:/../../var/www/"
 scp -i "$PRIVATE_KEY_LOCATION" ./nestjs-rest-api/dist/be.zip "$REMOTE_SERVER_USER@$REMOTE_SERVER_IP:/../../var/app/"
 
 echo 'Starting build script on remote server...'
-ssh -i .vagrant/machines/default/virtualbox/private_key "$REMOTE_SERVER_USER@$REMOTE_SERVER_IP" 'bash -s' < remote-build.sh
+ssh -i "$PRIVATE_KEY_LOCATION"  "$REMOTE_SERVER_USER@$REMOTE_SERVER_IP" 'bash -s' < remote-build.sh
